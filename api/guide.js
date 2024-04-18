c7b776eaeb510dc31a344f06ceb478257a4f6dea
Jason Miller <denzhengwei@163.com>
2024-04-18 17:46:47 +0800

    Update guide.js

--- api/guide.js
+++ api/guide.js
@@ -1,6 +1,4 @@
 export default async function handler(request, response) {
-    const host = request.headers.host;
-    console.log("host", host);
     const htmlContent = `404 Error`;
     response.setHeader('Content-Type', 'text/html');
     response.send(htmlContent);
